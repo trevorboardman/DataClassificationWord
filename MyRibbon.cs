5b7575409d2b23345284bb902b3a8e0d4c9ba0f6
Trevor Boardman <35644929+trevorboardman@users.noreply.github.com>
2019-09-09 13:07:41 -0400

    Add files via upload

---
+++ MyRibbon.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+using System.Drawing;
+
+
+
+
+
+namespace WordAddIn3
+{
+    [ComVisible(true)]
+    
+    public class MyRibbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+        
+        public string ReadDocumentProperty(string propertyName)
+        {
+            Microsoft.Office.Interop.Word.Document nativeDocument =Globals.ThisAddIn.Application.ActiveDocument;
+            Microsoft.Office.Tools.Word.Document vstoDocument = Globals.Factory.GetVstoObject(nativeDocument);
+            Office.DocumentProperties properties = (Office.DocumentProperties)vstoDocument.CustomDocumentProperties;
+
+            foreach (Office.DocumentProperty prop in properties)
+            {
+                if (prop.Name == propertyName)
+                {
+                    return prop.Value.ToString();
+                }
+            }
+            return null;
+        }
+
+        public void Confidential(Office.IRibbonControl control)
+        {
+
+            Microsoft.Office.Interop.Word.Document nativeDocument = Globals.ThisAddIn.Application.ActiveDocument;
+            Microsoft.Office.Tools.Word.Document vstoDocument = Globals.Factory.GetVstoObject(nativeDocument);
+            Office.DocumentProperties properties = (Office.DocumentProperties)vstoDocument.CustomDocumentProperties; 
+
+            if (ReadDocumentProperty("Classification") != null)
+            {
+                properties["Classification"].Delete();
+            }
+
+            properties.Add("Classification", false,
+                      Microsoft.Office.Core.MsoDocProperties.msoPropertyTypeString,
+                      "Confidential");
+            
+            foreach (Word.Section wordSection in Globals.ThisAddIn.Application.ActiveDocument.Sections)
+            {
+                
+                Word.Range footerRange = wordSection.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                footerRange.Font.ColorIndex = Word.WdColorIndex.wdDarkRed;
+                footerRange.Font.Size = 15;
+                footerRange.Text = "Classification: Confidential";
+                footerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                Word.Range headerRange = wordSection.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                headerRange.Font.ColorIndex = Word.WdColorIndex.wdDarkRed;
+                headerRange.Font.Size = 15;
+                headerRange.Text = "Classification: Confidential";
+                headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            }
+            
+        }
+        public void IUO(Office.IRibbonControl control)
+        {
+
+            Microsoft.Office.Interop.Word.Document nativeDocument = Globals.ThisAddIn.Application.ActiveDocument;
+            Microsoft.Office.Tools.Word.Document vstoDocument = Globals.Factory.GetVstoObject(nativeDocument);
+            Office.DocumentProperties properties = (Office.DocumentProperties)vstoDocument.CustomDocumentProperties;
+
+            if (ReadDocumentProperty("Classification") != null)
+            {
+                properties["Classification"].Delete();
+            }
+
+            properties.Add("Classification", false,
+                      Microsoft.Office.Core.MsoDocProperties.msoPropertyTypeString,
+                      "Internal");
+
+            foreach (Word.Section wordSection in Globals.ThisAddIn.Application.ActiveDocument.Sections)
+            {
+                Word.Range footerRange = wordSection.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                footerRange.Font.ColorIndex = Word.WdColorIndex.wdDarkBlue;
+                footerRange.Font.Size = 15;
+                footerRange.Text = "Classification: Internal Use Only";
+                footerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                Word.Range headerRange = wordSection.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                headerRange.Font.ColorIndex = Word.WdColorIndex.wdDarkBlue;
+                headerRange.Font.Size = 15;
+                headerRange.Text = "Classification: Internal Use Only";
+                headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            }
+        }
+
+        public void RemoveClassificationButton(Office.IRibbonControl control)
+        {
+
+            Microsoft.Office.Interop.Word.Document nativeDocument = Globals.ThisAddIn.Application.ActiveDocument;
+            Microsoft.Office.Tools.Word.Document vstoDocument = Globals.Factory.GetVstoObject(nativeDocument);
+            Office.DocumentProperties properties = (Office.DocumentProperties)vstoDocument.CustomDocumentProperties;
+
+            if (ReadDocumentProperty("Classification") != null)
+            {
+                properties["Classification"].Delete();
+            }
+
+            foreach (Word.Section wordSection in Globals.ThisAddIn.Application.ActiveDocument.Sections)
+            {
+                Word.Range footerRange = wordSection.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                footerRange.Delete();
+                Word.Range headerRange = wordSection.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                headerRange.Delete();
+            }
+
+        }
+
+        public void PublicUse(Office.IRibbonControl control)
+        {
+
+            Microsoft.Office.Interop.Word.Document nativeDocument = Globals.ThisAddIn.Application.ActiveDocument;
+            Microsoft.Office.Tools.Word.Document vstoDocument = Globals.Factory.GetVstoObject(nativeDocument);
+            Office.DocumentProperties properties = (Office.DocumentProperties)vstoDocument.CustomDocumentProperties;
+
+            if (ReadDocumentProperty("Classification") != null)
+            {
+                properties["Classification"].Delete();
+            }
+
+            properties.Add("Classification", false,
+                      Microsoft.Office.Core.MsoDocProperties.msoPropertyTypeString,
+                      "Public");
+
+            foreach (Word.Section wordSection in Globals.ThisAddIn.Application.ActiveDocument.Sections)
+            {
+                Word.Range footerRange = wordSection.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                footerRange.Font.ColorIndex = Word.WdColorIndex.wdGreen;
+                footerRange.Font.Size = 15;
+                footerRange.Text = "Classification: Public";
+                footerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                Word.Range headerRange = wordSection.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                headerRange.Font.ColorIndex = Word.WdColorIndex.wdGreen;
+                headerRange.Font.Size = 15;
+                headerRange.Text = "Classification: Public";
+                headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            }
+        }
+
+        public void PrivateUse(Office.IRibbonControl control)
+        {
+
+            Microsoft.Office.Interop.Word.Document nativeDocument = Globals.ThisAddIn.Application.ActiveDocument;
+            Microsoft.Office.Tools.Word.Document vstoDocument = Globals.Factory.GetVstoObject(nativeDocument);
+            Office.DocumentProperties properties = (Office.DocumentProperties)vstoDocument.CustomDocumentProperties;
+
+            if (ReadDocumentProperty("Classification") != null)
+            {
+                properties["Classification"].Delete();
+            }
+
+            properties.Add("Classification", false,
+                      Microsoft.Office.Core.MsoDocProperties.msoPropertyTypeString,
+                      "Private");
+
+            foreach (Word.Section wordSection in Globals.ThisAddIn.Application.ActiveDocument.Sections)
+            {
+                Word.Range footerRange = wordSection.Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                footerRange.Font.ColorIndex = Word.WdColorIndex.wdDarkYellow;
+                footerRange.Font.Size = 15;
+                footerRange.Text = "Classification: Private";
+                footerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                Word.Range headerRange = wordSection.Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                headerRange.Font.ColorIndex = Word.WdColorIndex.wdDarkYellow;
+                headerRange.Font.Size = 15;
+                headerRange.Text = "Classification: Private";
+                headerRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            }
+        }
+
+        public MyRibbon()
+        {
+           
+        }
+
+#region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("WordAddIn3.MyRibbon.xml");
+        }
+
+#endregion
+
+#region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+#endregion
+
+#region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+#endregion
+    }
+}
---
+++ ThisAddIn.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using System.Reflection;
+
+
+namespace WordAddIn3
+{
+    public partial class ThisAddIn
+    {
+
+        
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new MyRibbon();
+        }
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            
+            
+             this.Application.DocumentBeforeSave += new Word.ApplicationEvents4_DocumentBeforeSaveEventHandler(Application_DocumentBeforeSave);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+       private void DocumentBeforeSave()
+        {
+           
+
+        }
+
+        void Application_DocumentBeforeSave(Word.Document Doc, ref bool SaveAsUI, ref bool Cancel)
+        {
+            Microsoft.Office.Interop.Word.Document nativeDocument = Globals.ThisAddIn.Application.ActiveDocument;
+            Microsoft.Office.Tools.Word.Document vstoDocument = Globals.Factory.GetVstoObject(nativeDocument);
+            Microsoft.Office.Core.DocumentProperties properties = (Office.DocumentProperties)vstoDocument.CustomDocumentProperties;
+            string messageBoxText = "We noticed you haven't classified your document, would you like to opt out of classifying this document?";
+            string caption = "Classify Your Document";
+
+            object oBasic = Application.WordBasic;
+
+            object fIsAutoSave =
+             oBasic.GetType().InvokeMember(
+                "IsAutosaveEvent",
+             BindingFlags.GetProperty,
+                 null, oBasic, null);
+
+            MessageBoxButtons button = MessageBoxButtons.YesNo;
+            MessageBoxIcon icon = MessageBoxIcon.Warning;
+
+            if (ReadDocumentProperty2("Classification") == null && !(int.Parse(fIsAutoSave.ToString()) == 1))
+            {
+                DialogResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+                switch (result)
+                {
+                    case DialogResult.Yes:
+                        properties.Add("Classification", false,
+                       Microsoft.Office.Core.MsoDocProperties.msoPropertyTypeString,
+                       "Not Classified");
+                        break;
+                    case DialogResult.No:
+                        MessageBox.Show("Please add a classification and try saving the document again", "Classify", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Cancel = true;
+                        break;
+
+                }
+
+            }
+
+        }
+
+        public string ReadDocumentProperty2(string propertyName)
+        {
+            Microsoft.Office.Interop.Word.Document nativeDocument = Globals.ThisAddIn.Application.ActiveDocument;
+            Microsoft.Office.Tools.Word.Document vstoDocument = Globals.Factory.GetVstoObject(nativeDocument);
+            Office.DocumentProperties properties = (Office.DocumentProperties)vstoDocument.CustomDocumentProperties;
+
+            foreach (Office.DocumentProperty prop in properties)
+            {
+                if (prop.Name == propertyName)
+                {
+                    return prop.Value.ToString();
+                }
+            }
+            return null;
+        }
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+
+
+    public class DocumentClass 
+    {
+
+
+    }
+}
